fb958ab10e7d75b0fe7c2be7b349b927e824a844
Pavel Popchikovsky <ppopchikovsky@gmail.com>
2013-09-27 21:07:01 +0400

    index.php also shows table with total values

--- server/index.php
+++ server/index.php
@@ -2,6 +2,10 @@
 	include "auth.inc";
 	include "tools.inc";
 	connect();
+
+	$result = mysql_query("SELECT sum(count) as count, sum(time) as time, sum(peter) as peter, sum(colin) as colin from dice");
+	show_table($result, false);
+
 	$result = mysql_query("SELECT * from dice ORDER BY timestamp DESC LIMIT 1000");
-	show_table($result);
+	show_table($result, true);
 ?>
