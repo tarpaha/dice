a85123eb6ce35d337c493a338798ea6cd6e75aa5
Pavel Popchikovsky <ppopchikovsky@gmail.com>
2013-09-27 21:14:23 +0400

    showing of final result added to index.php

--- server/index.php
+++ server/index.php
@@ -3,9 +3,17 @@
 	include "tools.inc";
 	connect();
 
+	echo "Result:\n";
+	$result = mysql_query("SELECT sum(peter)/sum(count) as peter_win_probability from dice");
+	show_table($result, false);
+
+	echo "<br>\n";
+	echo "Total values:\n";
 	$result = mysql_query("SELECT sum(count) as count, sum(time) as time, sum(peter) as peter, sum(colin) as colin from dice");
 	show_table($result, false);
 
+	echo "<br>\n";
+	echo "Last 1000 tasks:\n";
 	$result = mysql_query("SELECT * from dice ORDER BY timestamp DESC LIMIT 1000");
 	show_table($result, true);
 ?>
